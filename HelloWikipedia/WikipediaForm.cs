1948b88ccbbfc3b57c500676514cafaf5f5f4add
yongan <shiya@300624.cn>
2022-08-23 21:05:55 +0800

    Init Commit

---
+++ HelloWikipedia/WikipediaForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HelloWikipedia
+{
+    public partial class WikipediaForm : Form
+    {
+        public WikipediaForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
